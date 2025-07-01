90473af9e090d00d16d1f37b9afe7234079d25e4
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-07-01 10:41:01 +0200

    Add Ampel and Ampelphase classes; implement traffic light functionality and simulation

---
+++ src/de/atsa/mit/loesung/PunktPunktPunkt.java
@@ -0,0 +1,20 @@
+package de.atsa.mit.loesung;
+
+public class PunktPunktPunkt {
+    public static void main(String[] args) {
+        System.out.println("der Mittelwert von 17 und 4 ist: " + mittelwertBrechnen(17, 4));
+        System.out.println("der mittelwert von 12, 5, und 22 ist: "+mittelertBrechnen(12,2,22));
+    }
+    public static int mittelertBrechnen(int eins, int zwei, int drei, int vier, int fuenf){
+        return (eins+zwei+drei+vier+fuenf)/4;
+    }
+    public static int mittelertBrechnen(int eins, int zwei, int drei, int vier){
+        return (eins+zwei+drei+vier)/4;
+    }
+    public static int mittelertBrechnen(int eins, int zwei, int drei){
+        return (eins+zwei+drei)/3;
+    }
+    public static int mittelwertBrechnen(int eins, int zwei){
+        return (eins+zwei)/2;
+    }
+}
--- src/de/atsa/mit/loesung/SwitchenImZooUndAufMission.java
+++ src/de/atsa/mit/loesung/SwitchenImZooUndAufMission.java
@@ -1,4 +1,42 @@
 package de.atsa.mit.loesung;
 
+import java.util.Random;
+
 public class SwitchenImZooUndAufMission {
+    public static void main(String[] args) {
+        System.out.println("Wilkommen beim Geheimdienst, wähle deine Mission\n\t" +
+                "1: Stehlen eins geheimen Dokuments\n\t" +
+                "2: Retten einer Geisel\n\t" +
+                "3: Ausschalten eines feindlichen Agenten\n\t" +
+                "Mit Jeder andere anderen Zahl geht du un den (unbezahlten) Urlaub");
+
+        Random wuerfel = new Random();
+        int awswahl = wuerfel.nextInt(1, 10);//
+        switch (awswahl){
+            case 1 -> System.out.println("Stehle die geheimn Dokumente von Dr. No");
+            case 2->System.out.println("Rette die entführte Prinzenssin");
+            case 3->{
+                System.out.println("Ein Bond wurde gesichtet, ist das der Richtig?");
+                System.out.println("Wenn es nicht James: schalte ihn aus");
+            }
+            case 4 -> System.out.println("Erstaunlich, das wae keine Option");
+            default -> System.out.println("Kein Agent geht jamals in den Urlaub: du bist enttarnt!");
+        }
+
+        System.out.println("\n Nach der Mission geht es in den Zoo");
+        String[] tiere = {"Löwe", "Tiger", "Hai", "Ara","Hund" };
+        int tierenIndex = wuerfel.nextInt(tiere.length);
+        String tier = tiere[tierenIndex];
+        String sound = switch (tier){
+          case "Hund" ->"Wau wau";
+          case "Ara" ->"Krächts";
+          case "Hai" ->"Blubb";
+            default -> "Dieser Tiersound wurde noch nicht aufgenommen";
+        };
+        System.out.println(tier + " macht " + sound);
+
+        System.out.println("\nNach dem Zoo gibt es Kaffee");
+        String bestellung = "Eiskaffee";
+        System.out.println("Du hast bestellt: " +bestellung+" ,dazufolgende Info:");
+    }
 }
