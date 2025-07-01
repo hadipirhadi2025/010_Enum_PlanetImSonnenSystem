c244acb005ec7ada9c7aaf6877afb77151a495c8
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-07-01 10:45:03 +0200

    Add method to calculate average from an array of integers

--- src/de/atsa/mit/loesung/PunktPunktPunkt.java
+++ src/de/atsa/mit/loesung/PunktPunktPunkt.java
@@ -4,6 +4,17 @@ public class PunktPunktPunkt {
     public static void main(String[] args) {
         System.out.println("der Mittelwert von 17 und 4 ist: " + mittelwertBrechnen(17, 4));
         System.out.println("der mittelwert von 12, 5, und 22 ist: "+mittelertBrechnen(12,2,22));
+        int[] zahlen = {170, 204, -1001};
+        System.out.println("Mittelwert: " + mittelertBrechnen(zahlen));
+    }
+
+    public static int mittelertBrechnen(int[] zahlen){
+        int summe = 0;
+        for (int i = 0; i < zahlen.length; i++) {
+            summe = summe + zahlen[i];
+
+        }
+        return  summe / zahlen.length;
     }
     public static int mittelertBrechnen(int eins, int zwei, int drei, int vier, int fuenf){
         return (eins+zwei+drei+vier+fuenf)/4;
