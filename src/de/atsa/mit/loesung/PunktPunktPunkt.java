4ca55408c8d0765cc1de56d86a306cb6da1189ab
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-07-01 10:51:24 +0200

    Refactor mittelertBrechnen method to use varargs for flexible input; remove redundant overloads

--- src/de/atsa/mit/loesung/PunktPunktPunkt.java
+++ src/de/atsa/mit/loesung/PunktPunktPunkt.java
@@ -2,13 +2,14 @@ package de.atsa.mit.loesung;
 
 public class PunktPunktPunkt {
     public static void main(String[] args) {
-        System.out.println("der Mittelwert von 17 und 4 ist: " + mittelwertBrechnen(17, 4));
+        System.out.println("der Mittelwert von 17 und 4 ist: " + mittelertBrechnen(17, 4));
         System.out.println("der mittelwert von 12, 5, und 22 ist: "+mittelertBrechnen(12,2,22));
         int[] zahlen = {170, 204, -1001};
         System.out.println("Mittelwert: " + mittelertBrechnen(zahlen));
+        System.out.println("Mittelwert: "+ mittelertBrechnen(1,67,503,-4, 155, 202, -1001, 200));
     }
 
-    public static int mittelertBrechnen(int[] zahlen){
+    public static int mittelertBrechnen(int ... zahlen){
         int summe = 0;
         for (int i = 0; i < zahlen.length; i++) {
             summe = summe + zahlen[i];
@@ -16,16 +17,25 @@ public class PunktPunktPunkt {
         }
         return  summe / zahlen.length;
     }
-    public static int mittelertBrechnen(int eins, int zwei, int drei, int vier, int fuenf){
-        return (eins+zwei+drei+vier+fuenf)/4;
-    }
-    public static int mittelertBrechnen(int eins, int zwei, int drei, int vier){
-        return (eins+zwei+drei+vier)/4;
-    }
-    public static int mittelertBrechnen(int eins, int zwei, int drei){
-        return (eins+zwei+drei)/3;
-    }
-    public static int mittelwertBrechnen(int eins, int zwei){
-        return (eins+zwei)/2;
-    }
+
+//    public static int mittelertBrechnen(int[] zahlen){
+//        int summe = 0;
+//        for (int i = 0; i < zahlen.length; i++) {
+//            summe = summe + zahlen[i];
+//
+//        }
+//        return  summe / zahlen.length;
+//    }
+//    public static int mittelertBrechnen(int eins, int zwei, int drei, int vier, int fuenf){
+//        return (eins+zwei+drei+vier+fuenf)/4;
+//    }
+//    public static int mittelertBrechnen(int eins, int zwei, int drei, int vier){
+//        return (eins+zwei+drei+vier)/4;
+//    }
+//    public static int mittelertBrechnen(int eins, int zwei, int drei){
+//        return (eins+zwei+drei)/3;
+//    }
+//    public static int mittelertBrechnen(int eins, int zwei){
+//        return (eins+zwei)/2;
+//    }
 }
